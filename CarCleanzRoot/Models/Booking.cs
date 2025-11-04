b56221a07ea761da7daf1f98b2469f4298439dd1
arukmr1990-sketch <arukmr1990@gmail.com>
2025-11-04 19:43:47 +0530

    Update Booking.cs

--- CarCleanzRoot/Models/Booking.cs
+++ CarCleanzRoot/Models/Booking.cs
@@ -1,1 +1,13 @@
+namespace CarCleanzApp.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string CarType { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
 
