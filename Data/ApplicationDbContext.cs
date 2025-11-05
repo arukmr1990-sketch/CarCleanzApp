f6c95d7c9bcd40cef387c6000edce2fc9ec83994
Arun Kumar <arukmr1990@gmail.com>
2025-11-05 16:02:52 +0530

    Initial Car Cleanz Project Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using CarCleanz.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CarCleanz.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<AdminView> AdminViews { get; set; }
+    }
+}
+
---
+++ Models/AdminView.cs
@@ -0,0 +1,11 @@
+
+namespace CarCleanz.Models
+{
+    public class AdminView
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
