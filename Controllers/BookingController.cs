f82642ac29bb762c10fef5c7c8dfc5b92eab7518
Arun Kumar <arukmr1990@gmail.com>
2025-11-10 10:45:52 +0530

    Added admin login and session protection

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -2,61 +2,57 @@ using Microsoft.AspNetCore.Mvc;
 using CarCleanz.Models;
 using CarCleanz.Data;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace CarCleanzApp.Controllers
 {
-    public class BookingController : Controller
+   public class BookingController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public BookingController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public BookingController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult Index()
-        {
-            return View("Create");
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Booking model)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Bookings.Add(model);
-                _context.SaveChanges();
-
-                ViewBag.Name = model.Name;
-                ViewBag.Email = model.Email;
-                ViewBag.VehicleType = model.VehicleType;
-                ViewBag.ServicePackage = model.ServicePackage;
-                ViewBag.BookingDate = model.BookingDate.ToString("dd-MM-yyyy");
-                ViewBag.Phone = model.Phone;
-
-                return View("Success");
-            }
-
-            return View(model);
-        }
-
-        public IActionResult Success()
-        {
-            return View();
-        }
-
-        public IActionResult Admin()
-        {
-            // ? Fetch all bookings from SQLite
-            var bookings = _context.Bookings.ToList();
-            return View(bookings);
-        }
+        _context = context;
     }
+
+    // ? Login (GET)
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    // ? Login (POST)
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "carcleanz@2025")
+        {
+            HttpContext.Session.SetString("AdminLoggedIn", "true");
+            return RedirectToAction("Admin");
+        }
+
+        ViewBag.Error = "Invalid username or password";
+        return View();
+    }
+
+    // ? Logout
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Remove("AdminLoggedIn");
+        return RedirectToAction("Login");
+    }
+
+    // ? Admin page
+    public IActionResult Admin()
+    {
+        if (HttpContext.Session.GetString("AdminLoggedIn") != "true")
+        {
+            return RedirectToAction("Login");
+        }
+
+        var bookings = _context.Bookings.ToList();
+        return View(bookings);
+    }
+}
 }
