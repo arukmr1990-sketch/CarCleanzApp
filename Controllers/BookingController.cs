2f0ef4abdb198b9420a2393beba11f09b170136d
Arun Kumar <arukmr1990@gmail.com>
2025-11-10 12:44:18 +0530

    Fix Booking form POST method 405 error

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -4,64 +4,41 @@ using CarCleanz.Data;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 
-namespace CarCleanzApp.Controllers
+namespace CarCleanz.Controllers
 {
-   public class BookingController : Controller
-{
-    private readonly ApplicationDbContext _context;
-
-    public BookingController(ApplicationDbContext context)
+    public class BookingController : Controller
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    // ? Login (GET)
-    [HttpGet]
-    public IActionResult Login()
-    {
-        return View();
-    }
-
-    // ? Login (POST)
-    [HttpPost]
-    public IActionResult Login(string username, string password)
-    {
-        if (username == "admin" && password == "carcleanz@2025")
+        public BookingController(ApplicationDbContext context)
         {
-            HttpContext.Session.SetString("AdminLoggedIn", "true");
-            return RedirectToAction("Admin");
+            _context = context;
         }
 
-        ViewBag.Error = "Invalid username or password";
-        return View();
-    }
-
-    // ? Logout
-    public IActionResult Logout()
-    {
-        HttpContext.Session.Remove("AdminLoggedIn");
-        return RedirectToAction("Login");
-    }
-[HttpGet]
-public IActionResult Create()
-{
-    return View();
-}
-public IActionResult Index()
-{
-    return RedirectToAction("Create");
-}
-
-    // ? Admin page
-    public IActionResult Admin()
-    {
-        if (HttpContext.Session.GetString("AdminLoggedIn") != "true")
+        // ? GET: /Booking/Create
+        [HttpGet]
+        public IActionResult Create()
         {
-            return RedirectToAction("Login");
+            return View();
         }
 
-        var bookings = _context.Bookings.ToList();
-        return View(bookings);
+        // ? POST: /Booking/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Bookings.Add(booking);
+                _context.SaveChanges();
+                return RedirectToAction("Success");
+            }
+            return View(booking);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
     }
-}
 }
